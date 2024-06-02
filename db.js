11334fe25a64c36239c708adf37aa59d84030eab
Bad-Goty <165513433+Bad-Goty@users.noreply.github.com>
2024-06-02 16:57:06 -0600

    Update db.js

--- db.js
+++ db.js
@@ -40,42 +40,27 @@ app.put('/api/updateRole', (req, res) => {
   });
 });
 
-app.delete('/api/deleteUserAndCards/:email', (req, res) => {
+app.delete('/api/deleteTarjeta/:email', (req, res) => {
   const email = req.params.email;
-
-  pool.getConnection((err, connection) => {
+  pool.query('DELETE FROM tarjetas_usuario WHERE usuario_email = ?', [email], (err, results) => {
     if (err) {
-      console.error('Error obteniendo la conexión:', err);
-      return res.status(500).json({ error: 'Error en la base de datos' });
+      console.error('Error ejecutando la consulta:', err);
+      res.status(500).json({ error: 'Error en la base de datos' });
+    } else {
+      res.json({ message: 'Tarjetas eliminadas correctamente' });
     }
+  });
+});
 
-    connection.beginTransaction(async (err) => {
-      if (err) {
-        connection.release();
-        console.error('Error iniciando la transacción:', err);
-        return res.status(500).json({ error: 'Error en la base de datos' });
-      }
-
-      try {
-        // Eliminar los registros relacionados en la tabla tarjetas_usuario
-        await connection.query('DELETE FROM tarjetas_usuario WHERE usuario_email = ?', [email]);
-
-        // Eliminar el usuario en la tabla usuarios
-        await connection.query('DELETE FROM usuarios WHERE email = ?', [email]);
-
-        // Confirmar la transacción si todas las operaciones se realizaron correctamente
-        await connection.commit();
-        res.json({ message: 'Usuario y tarjetas eliminados correctamente' });
-      } catch (err) {
-        // Revertir la transacción si ocurrió un error
-        await connection.rollback();
-        console.error('Error ejecutando la consulta:', err);
-        res.status(500).json({ error: 'Error en la base de datos' });
-      } finally {
-        // Liberar la conexión
-        connection.release();
-      }
-    });
+app.delete('/api/deleteUser/:email', (req, res) => {
+  const email = req.params.email;
+  pool.query('DELETE FROM usuarios WHERE email = ?', [email], (err, results) => {
+    if (err) {
+      console.error('Error ejecutando la consulta:', err);
+      res.status(500).json({ error: 'Error en la base de datos' });
+    } else {
+      res.json({ message: 'Usuario eliminado correctamente' });
+    }
   });
 });
 
