ab6337044429bb49c4918f45cd79e32916253568
Bad-Goty <165513433+Bad-Goty@users.noreply.github.com>
2024-06-02 16:45:29 -0600

    Update db.js

--- db.js
+++ db.js
@@ -40,43 +40,41 @@ app.put('/api/updateRole', (req, res) => {
   });
 });
 
-app.delete('/api/deleteUser/:email', (req, res) => {
+app.delete('/api/deleteUser/:email', async (req, res) => {
   const email = req.params.email;
 
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error obteniendo la conexión:', err);
-      return res.status(500).json({ error: 'Error en la base de datos' });
+  let connection;
+
+  try {
+    // Intentar obtener una conexión del pool
+    connection = await pool.getConnection();
+
+    // Iniciar una transacción
+    await connection.beginTransaction();
+
+    // Eliminar los registros relacionados en la tabla tarjetas_usuario
+    await connection.query('DELETE FROM tarjetas_usuario WHERE usuario_email = ?', [email]);
+
+    // Eliminar el usuario en la tabla usuarios
+    await connection.query('DELETE FROM usuarios WHERE email = ?', [email]);
+
+    // Confirmar la transacción si todas las operaciones se realizaron correctamente
+    await connection.commit();
+
+    res.json({ message: 'Usuario y tarjetas eliminados correctamente' });
+  } catch (err) {
+    // Revertir la transacción si ocurrió un error
+    if (connection) {
+      await connection.rollback();
     }
-
-    connection.beginTransaction(async (err) => {
-      if (err) {
-        connection.release();
-        console.error('Error iniciando la transacción:', err);
-        return res.status(500).json({ error: 'Error en la base de datos' });
-      }
-
-      try {
-        // Eliminar los registros relacionados en la tabla tarjetas_usuario
-        await connection.query('DELETE FROM tarjetas_usuario WHERE usuario_email = ?', [email]);
-
-        // Eliminar el usuario en la tabla usuarios
-        await connection.query('DELETE FROM usuarios WHERE email = ?', [email]);
-
-        // Confirmar la transacción si todas las operaciones se realizaron correctamente
-        await connection.commit();
-        res.json({ message: 'Usuario y tarjetas eliminados correctamente' });
-      } catch (err) {
-        // Revertir la transacción si ocurrió un error
-        await connection.rollback();
-        console.error('Error ejecutando la consulta:', err);
-        res.status(500).json({ error: 'Error en la base de datos' });
-      } finally {
-        // Liberar la conexión
-        connection.release();
-      }
-    });
-  });
+    console.error('Error ejecutando la consulta:', err);
+    res.status(500).json({ error: 'Error en la base de datos' });
+  } finally {
+    // Liberar la conexión si existe
+    if (connection) {
+      connection.release();
+    }
+  }
 });
 
 app.get('/api/getPerfumes', (req, res) => {
